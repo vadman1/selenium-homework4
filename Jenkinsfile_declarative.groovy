8c272b646cf868c45b8c99b9ab40ada326ccc695
vadman1 <vsam1998@mail.ru>
2021-08-17 02:14:43 +0500

    add Jenkinsfile

---
+++ Jenkinsfile_declarative.groovy
@@ -0,0 +1,32 @@
+def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
+
+pipeline {
+    agent any
+    parameters {
+        string(name: 'TAG', defaultValue: '@firstTest', description: 'тег для запуска')
+    }
+    stages {
+        stage('Build') {
+            steps {
+                sh "${mvn} clean compile"
+            }
+        }
+        stage('Run Tests') {
+            steps {
+                sh "${mvn} test -Dcucumber.filter.tags=\"${params.TAG}\""
+            }
+        }
+        stage('Allure Report Generation') {
+            steps {
+                allure includeProperties: false,
+                        jdk: '',
+                        results: [[path: 'target/reports/allure-results']]
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs notFailBuild: true
+        }
+    }
+}
