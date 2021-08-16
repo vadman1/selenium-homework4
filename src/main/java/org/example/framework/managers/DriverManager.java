5f54fb5dbdb90ab7662da40bb68008116e4a3768
vadman1 <vsam1998@mail.ru>
2021-08-17 03:06:31 +0500

    add remote browser

--- src/main/java/org/example/framework/managers/DriverManager.java
+++ src/main/java/org/example/framework/managers/DriverManager.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 import static org.example.framework.utils.PropConst.*;
 
@@ -133,6 +138,23 @@ public class DriverManager {
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", props.getProperty(chrome));
                 driver = new ChromeDriver();
+                break;
+            case "remote":
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setBrowserName("chrome");
+                capabilities.setVersion("73.0");
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", false);
+
+                try {
+                    RemoteWebDriver driver = new RemoteWebDriver(
+                            URI.create("http://:4445/wd/hub").toURL(),
+                            capabilities
+                    );
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
                 break;
             default:
                 Assertions.fail("Типа браузера '" + props.getProperty(TYPE_BROWSER) + "' не существует во фреймворке");
