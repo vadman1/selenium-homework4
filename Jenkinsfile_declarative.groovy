395cb1dc58b5f4b42565599e2372d13ece2355a5
vadman1 <vsam1998@mail.ru>
2021-08-17 14:18:51 +0500

    agent any

--- Jenkinsfile_declarative.groovy
+++ Jenkinsfile_declarative.groovy
@@ -1,7 +1,7 @@
 def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
 
 pipeline {
-    agent { label 'linux' }
+    agent any
     parameters {
         string(name: 'TAG', defaultValue: '@firstTest', description: 'тег для запуска')
     }
